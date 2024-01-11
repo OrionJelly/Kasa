d3022c03b9166a883a9bd59f8e78bbb9f1532503
OrionJelly <91801372+OrionJelly@users.noreply.github.com>
2024-01-11 14:04:16 +0100

    fix : Correction path redirection

--- src/pages/Rent/index.jsx
+++ src/pages/Rent/index.jsx
@@ -11,6 +11,9 @@ import Host from '../../components/Host'
 import Rating from '../../components/Rating'
 import Rentlabel from '../../components/Rentlabel'
 
+
+
+
 // le pros {list} correspond au tableau de locations
 function Rent({ list }) {
     // Utilisation d'useParams pour récupérer l'id de la location dans l'URL
@@ -19,50 +22,53 @@ function Rent({ list }) {
     // Const qui retourne l'objet qui correspond à l'id de la location
     const currentRentId = list.find((arr) => arr.id === rentId.rentId)
 
+
     return (
         <>
-    {/* Condition booléenne si currentRendId = true retourne les données de la location, sinon renvoi sur la page d'erreur */}
-        {currentRentId ?
-            <main className="main-container">
-            <Slide
-            pictures={currentRentId.pictures}
-            alt={currentRentId.title}
-            />
-            <section className="section-first">
-            <Rentlabel
-            title={currentRentId.title}
-            content={currentRentId.location}
-            />
-            <div className="section-first__tags">
-            {currentRentId.tags.map((tag, i) => (
-                <Tags key={i} content={tag} />
-                ))}
-                </div>
-                </section>
-                <section className="section-second">
-                <Host
-                name={currentRentId.host.name}
-                picture={currentRentId.host.picture}
-                />
-                <Rating rating={currentRentId.rating} />
-                </section>
-                <section className="section-third">
-                <Collapse
-                title="Description"
-                text={currentRentId.description}
-                />
-                <Collapse
-                title="Équipements"
-                text={currentRentId.equipments.map((equip, index) => (
-                    <li key={index}>{equip}</li>
-                    ))}
+            {/* Condition booléenne si currentRendId = true retourne les données de la location, sinon renvoi sur la page d'erreur */}
+            {currentRentId ? (
+                <main className="main-container">
+                    <Slide
+                        pictures={currentRentId.pictures}
+                        alt={currentRentId.title}
                     />
+                    <section className="section-first">
+                        <Rentlabel
+                            title={currentRentId.title}
+                            content={currentRentId.location}
+                        />
+                        <div className="section-first__tags">
+                            {currentRentId.tags.map((tag, i) => (
+                                <Tags key={i} content={tag} />
+                            ))}
+                        </div>
                     </section>
-                    </main>
-                : <Navigate to="*"/>}
-                </>
-
-
+                    <section className="section-second">
+                        <Host
+                            name={currentRentId.host.name}
+                            picture={currentRentId.host.picture}
+                        />
+                        <Rating rating={currentRentId.rating} />
+                    </section>
+                    <section className="section-third">
+                        <Collapse
+                            title="Description"
+                            text={currentRentId.description}
+                        />
+                        <Collapse
+                            title="Équipements"
+                            text={currentRentId.equipments.map(
+                                (equip, index) => (
+                                    <li key={index}>{equip}</li>
+                                )
+                            )}
+                        />
+                    </section>
+                </main>
+            ) : (
+                <Navigate to="../*" />
+            )}
+        </>
     )
 }
 
